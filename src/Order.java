84aa907258743d9bbdb8d39ffbeee4ddcda9aff2
Ryan Stewart <97697313+stewryan@users.noreply.github.com>
2022-04-06 13:05:04 -0400

    Renamed Menu to Order, added getTotalPrice

--- src/Order.java
+++ src/Order.java
@@ -1,5 +1,30 @@
-public class Order {
-    
-    
+import java.util.ArrayList;
 
+public class Order {
+
+    public ArrayList<MenuItem> items = new ArrayList<>();
+
+    public void add(MenuItem item) {
+        items.add(item);
+    }
+
+    @Override
+    public String toString() {
+        String menu = "";
+
+        for (int i = 0; i < items.size(); i++) {
+            menu += "($" + items.get(i).getPrice() + ") Item [" + items.get(i).getID() + "]: " + items.get(i) + "\n";
+        }
+
+        return menu;
+    }
+
+    public double getTotalPrice() {
+        double total = 0;
+        for (int i = 0; i < items.size(); i++) {
+            total += items.get(i).getPrice();
+        }
+        return total;
+    }
+    
 }
--- src/Tester.java
+++ src/Tester.java
@@ -2,17 +2,18 @@ public class Tester {
 
     public static void main(String[] args) {
 
-        Menu menu = new Menu();
+        Order order = new Order();
 
         Drink smallPepsi = new Drink(45, "Pepsi (S)", 2.49, 0.98, Size.SMALL, 85);
         Drink mediumPepsi = new Drink(46, "Pepsi (M)", 4.49, 0.94, Size.MEDIUM, 140);
         Drink largePepsi = new Drink(47, "Pepsi (L)", 7.49, 0.92, Size.LARGE, 275);
 
-        menu.add(smallPepsi);
-        menu.add(mediumPepsi);
-        menu.add(largePepsi);
+        order.add(smallPepsi);
+        order.add(mediumPepsi);
+        order.add(largePepsi);
 
-        System.out.println(menu);
+        System.out.println(order);
+        System.out.println(order.getTotalPrice());
 
     }
     
